68b2c9fe5704b177e499d76ca8762774384cb00d
Samuel Scerri <samuelscerri533@gmail.com>
2024-07-14 18:14:09 +0200

    Very Teensy Tiny Change Again!

--- commands/utility/gubotamize.js
+++ commands/utility/gubotamize.js
@@ -5,7 +5,6 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("gubotamize")
     .setDescription("Wait, what are you doing?"),
-
   async execute(interaction) {
     await gptReset();
     await interaction.reply("AHHHHH");
