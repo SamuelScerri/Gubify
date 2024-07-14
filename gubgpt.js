eb729396754fc7003b321a821cdbf0f660ec3c49
Samuel Scerri <samuelscerri533@gmail.com>
2024-07-14 13:43:02 +0200

    Bot Can Now Remember Previous Convos

--- gubgpt.js
+++ gubgpt.js
@@ -1,9 +1,14 @@
 const { OpenAI } = require("openai");
 const openai = new OpenAI();
 
+let thread = null;
+
 module.exports = {
   async gptReply(message, name) {
-    const thread = await openai.beta.threads.create();
+    if (thread == null) {
+      console.log("No thread found! Creating new thread");
+      thread = await openai.beta.threads.create();
+    }
 
     const newMessage = await openai.beta.threads.messages.create(thread.id, {
       role: "user",
@@ -17,14 +22,7 @@ module.exports = {
 
     if (run.status === "completed") {
       const messages = await openai.beta.threads.messages.list(run.thread_id);
-      let response = "";
-
-      for (const message of messages.data.reverse()) {
-        if (message.role === "assistant")
-          response += `${message.content[0].text.value}\n`;
-      }
-
-      return response;
+      return messages.data[0].content[0].text.value;
     }
   },
 };
