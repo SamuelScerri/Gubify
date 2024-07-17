071e2d12213e749eef4030c6d2dcfb45e6966ba1
Samuel Scerri <samuelscerri533@gmail.com>
2024-07-17 18:52:12 +0200

    Cost Saving Measures Hehe
    GPT-4o is quite expensive, so its only used to describe images, which is then fed into GPT-3.5 Turbo. Result isn't as impressive as the original version but it's much cheaper to run!

--- gubgpt.js
+++ gubgpt.js
@@ -3,18 +3,32 @@ const openai = new OpenAI();
 
 let thread = null;
 
+export async function gptDescribe(attachments) {
+  if (attachments.length == 0) return "";
+
+  let run = await openai.beta.threads.createAndRunPoll({
+    assistant_id: "asst_P9BDKKhIoUW9M7vHY0NdkeS8",
+    thread: {
+      messages: [{ role: "user", content: attachments }],
+    },
+  });
+
+  const messages = await openai.beta.threads.messages.list(run.thread_id);
+
+  openai.beta.threads.del(run.thread_id);
+  return messages.data[0].content[0].text.value;
+}
+
 export async function gptReply(message) {
   if (thread == null) {
     console.log("No thread found! Creating new thread");
     thread = await openai.beta.threads.create();
   }
 
-  let content = [
-    { type: "text", text: `(user: ${message.name}) ${message.content}.` },
-  ];
+  let attachments = [];
 
   message.attachments.forEach((attached) => {
-    content.push({
+    attachments.push({
       type: "image_url",
       image_url: {
         url: attached.attachment,
@@ -25,9 +39,11 @@ export async function gptReply(message) {
   await message.interaction.channel.sendTyping();
 
   try {
+    const attachmentDescription = await gptDescribe(attachments);
+
     const newMessage = await openai.beta.threads.messages.create(thread.id, {
       role: "user",
-      content: content,
+      content: `(Image Description: ${attachmentDescription}) (User: ${message.name}) ${message.content}.`,
     });
 
     let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
@@ -37,6 +53,8 @@ export async function gptReply(message) {
     const messages = await openai.beta.threads.messages.list(run.thread_id);
     await message.interaction.reply(messages.data[0].content[0].text.value);
   } catch (error) {
+    console.error(error);
+
     await message.interaction.reply(
       "WARNING: GUB GUB SIMULATION DETECTED FOREIGN CONTENT, PLEASE TRY AGAIN",
     );
