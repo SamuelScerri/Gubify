f12c2cffc0be281a1cd1f00f488db7bf87a1637e
Samuel Scerri <samuelscerri533@gmail.com>
2024-07-19 08:52:35 +0200

    Incredibly Good Timing Regarding GPT 4o Mini,

--- gubgpt.js
+++ gubgpt.js
@@ -3,32 +3,18 @@ const openai = new OpenAI();
 
 let thread = null;
 
-export async function gptDescribe(attachments) {
-  if (attachments.length == 0) return "";
-
-  let run = await openai.beta.threads.createAndRunPoll({
-    assistant_id: "asst_P9BDKKhIoUW9M7vHY0NdkeS8",
-    thread: {
-      messages: [{ role: "user", content: attachments }],
-    },
-  });
-
-  const messages = await openai.beta.threads.messages.list(run.thread_id);
-
-  openai.beta.threads.del(run.thread_id);
-  return messages.data[0].content[0].text.value;
-}
-
 export async function gptReply(message) {
   if (thread == null) {
     console.log("No thread found! Creating new thread");
     thread = await openai.beta.threads.create();
   }
 
-  let attachments = [];
+  let content = [
+    { type: "text", text: `(user: ${message.name}) ${message.content}` },
+  ];
 
   message.attachments.forEach((attached) => {
-    attachments.push({
+    content.push({
       type: "image_url",
       image_url: {
         url: attached.attachment,
@@ -39,11 +25,9 @@ export async function gptReply(message) {
   await message.interaction.channel.sendTyping();
 
   try {
-    const attachmentDescription = await gptDescribe(attachments);
-
     const newMessage = await openai.beta.threads.messages.create(thread.id, {
       role: "user",
-      content: `[${message.name}] [${attachmentDescription}] [${message.content}]`,
+      content: content,
     });
 
     let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
